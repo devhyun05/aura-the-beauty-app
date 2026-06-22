d7f9c17430b933a4f7b0b3535f33d4bb0935c870
leejunhee8235 <leejunhee8235@gmail.com>
2026-06-23 04:50:33 +0900

    fix: 하트 아이콘과 분석 썸네일 정렬 수정

--- apps/mobile/src/features/profile/components/HeartIcon.tsx
+++ apps/mobile/src/features/profile/components/HeartIcon.tsx
@@ -1,45 +1,20 @@
-import { StyleSheet } from 'react-native';
-import { View } from 'tamagui';
+import { Image, StyleSheet } from 'react-native';
 
-import { userPageColors } from '../../../shared/theme/tokens';
+const heartFilled = require('../../../assets/icons/profile/heart-filled.png');
 
 export const HeartIcon = () => {
   return (
-    <View pointerEvents="none" style={styles.root}>
-      <View style={[styles.circle, styles.leftCircle]} />
-      <View style={[styles.circle, styles.rightCircle]} />
-      <View style={styles.diamond} />
-    </View>
+    <Image
+      resizeMode="contain"
+      source={heartFilled}
+      style={styles.icon}
+    />
   );
 };
 
 const styles = StyleSheet.create({
-  circle: {
-    backgroundColor: userPageColors.text,
-    borderRadius: 5,
-    height: 9,
-    position: 'absolute',
-    top: 1,
-    width: 9,
-  },
-  diamond: {
-    backgroundColor: userPageColors.text,
-    height: 9,
-    left: 2.5,
-    position: 'absolute',
-    top: 5,
-    transform: [{ rotate: '45deg' }],
-    width: 9,
-  },
-  leftCircle: {
-    left: 0,
-  },
-  rightCircle: {
-    right: 0,
-  },
-  root: {
+  icon: {
     height: 15,
-    position: 'relative',
     width: 16,
   },
 });
